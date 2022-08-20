2575af730528ca02cf14f430e814ed371e0fe612
Patryk Miklosz <patryk.miklosz@gmal.com>
2022-08-21 01:32:33 +0200

    Add AutoMapper service to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using RestaurantAPI.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<RestaurantDbContext>();
-
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
